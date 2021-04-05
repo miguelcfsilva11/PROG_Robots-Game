a10aafe8d74a949f5ba1c5c61484065f38a32a41
Miguel Silva <miguelcfsilva@gmail.com>
2021-04-05 15:26:01 +0100

    Updated Main Function

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,21 @@
 
+#include "file_reader.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    char key;
+	vector<string> maze;
+	string fileName = "mazes\MAZE_01.TXT";
+    
+    cout << "Press a key to load Maze 1!";
+    cin >> key;
+
+	fillVector(fileName, maze);
+
+}
