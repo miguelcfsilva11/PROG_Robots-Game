2230c7a68c78a51029234a06b40c616b6d77dd17
Miguel Silva <miguelcfsilva@gmail.com>
2021-04-05 15:25:22 +0100

    Created Header File

---
+++ src/file_reader.h
@@ -0,0 +1,11 @@
+#ifndef FILE_READER_H
+#define FILE_READER_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+void fillVector(std::string fileName, std::vector<std::string>& maze);
+
+#endif
