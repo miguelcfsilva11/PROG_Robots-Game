87608a2612d54b633e0ddeb4bf61ea9ff3897593
Miguel Silva <miguelcfsilva@gmail.com>
2021-04-06 14:31:24 +0100

    Deleted Maze Directory

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ int main()
 
     char key;
 	vector<string> maze;
-	string fileName = "../mazes/MAZE_01.txt";
+	string fileName = "MAZE_01.txt";
 
     cout << "Press a key to load Maze 1: ";
     cin >> key;
